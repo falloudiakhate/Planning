ea3745cd2beff4f1d26dac5bffac85e3b1598793
falloudiakhate <falloudiakhate9@gmail.com>
2020-01-25 23:24:45 +0000

    Database mysql Settings

--- Planning/settings.py
+++ Planning/settings.py
@@ -74,10 +74,13 @@ WSGI_APPLICATION = 'Planning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
