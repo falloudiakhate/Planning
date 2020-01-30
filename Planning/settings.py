8119a7b9d3f33f3de3bc8c3e7f905c0a69c10445
falloudiakhate <falloudiakhate9@gmail.com>
2020-01-30 17:30:37 +0000

    Database Config

--- Planning/settings.py
+++ Planning/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
                 'default': {
                 'ENGINE': 'django.db.backends.mysql',
                 'NAME': 'PlanningDatabase',
-                'USER': 'root',
+                'USER': 'django',
                 'PASSWORD': 'root',
                 'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
