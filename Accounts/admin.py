d4e7bda1374d20d89e80e45058dee5f0d4f58eab
mamediarra-sow <sow.mamediarra@live.fr>
2020-02-15 11:14:49 +0000

    pour l inscription

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -3,15 +3,10 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Utilisateur)
-admin.site.register(Professeur)
-admin.site.register(Eleve)
 admin.site.register(UE)
 admin.site.register(EC)
 admin.site.register(Maquette)
 admin.site.register(Time_Table)
-admin.site.register(Professeur_Pedagogique)
-admin.site.register(Chef_Departement)
 admin.site.register(Cahier_De_Texte)
 admin.site.register(Dispenser_Cours)
 admin.site.register(Publication)
-admin.site.register(Responsable)
---
+++ Accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from Accounts.models import Utilisateur  
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+Function=["eleve","professeur","responsable","responsable_pedagodique","chef_departement"]
+class  UserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=("username","first_name","last_name","email")
+class formInscription(forms.ModelForm): 
+    class Meta:
+        model = Utilisateur
+        fields = ("telephone","fonction")       
+        widget={
+           "fonction": forms.CheckboxSelectMultiple(choices=Function)
+        }
+       
+
